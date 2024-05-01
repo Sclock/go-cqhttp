36f5d77b96cc9e0ef5245996e255b15ce8215c8d
sclock <1342810270@qq.com>
2024-05-02 04:12:02 +0800

    检测到企点协议跳过签名流程

--- cmd/gocq/main.go
+++ cmd/gocq/main.go
@@ -163,27 +163,31 @@ func LoginInteract() {
 			log.Fatalf("加载设备信息失败: %v", err)
 		}
 	}
-	signServer, err := getAvaliableSignServer() // 获取可用签名服务器
-	if err != nil {
-		log.Warn(err)
-	}
-	if signServer != nil && len(signServer.URL) > 1 {
-		log.Infof("使用签名服务器：%v", signServer.URL)
-		go signStartRefreshToken(base.Account.RefreshInterval) // 定时刷新 token
-		wrapper.DandelionEnergy = energy
-		wrapper.FekitGetSign = sign
-		if !base.IsBelow110 {
-			if !base.Account.AutoRegister {
-				log.Warn("自动注册实例已关闭，请配置 sign-server 端自动注册实例以保持正常签名")
-			}
-			if !base.Account.AutoRefreshToken {
-				log.Info("自动刷新 token 已关闭，token 过期后获取签名时将不会立即尝试刷新获取新 token")
+
+	// 企点无需签名服务
+	if device.Protocol != 4 {
+		signServer, err := getAvaliableSignServer() // 获取可用签名服务器
+		if err != nil {
+			log.Warn(err)
+		}
+		if signServer != nil && len(signServer.URL) > 1 {
+			log.Infof("使用签名服务器：%v", signServer.URL)
+			go signStartRefreshToken(base.Account.RefreshInterval) // 定时刷新 token
+			wrapper.DandelionEnergy = energy
+			wrapper.FekitGetSign = sign
+			if !base.IsBelow110 {
+				if !base.Account.AutoRegister {
+					log.Warn("自动注册实例已关闭，请配置 sign-server 端自动注册实例以保持正常签名")
+				}
+				if !base.Account.AutoRefreshToken {
+					log.Info("自动刷新 token 已关闭，token 过期后获取签名时将不会立即尝试刷新获取新 token")
+				}
+			} else {
+				log.Warn("签名服务器版本 <= 1.1.0 ，无法使用刷新 token 等操作，建议使用 1.1.6 版本及以上签名服务器")
 			}
 		} else {
-			log.Warn("签名服务器版本 <= 1.1.0 ，无法使用刷新 token 等操作，建议使用 1.1.6 版本及以上签名服务器")
+			log.Warnf("警告: 未配置签名服务器或签名服务器不可用, 这可能会导致登录 45 错误码或发送消息被风控")
 		}
-	} else {
-		log.Warnf("警告: 未配置签名服务器或签名服务器不可用, 这可能会导致登录 45 错误码或发送消息被风控")
 	}
 
 	if base.Account.Encrypt {
